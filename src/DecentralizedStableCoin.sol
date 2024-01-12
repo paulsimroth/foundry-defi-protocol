9932e7f393816b5be5642a0c56c2477c1ec740c0
paulsimroth <mail@simroth.at>
2024-01-12 18:36:08 +0100

    stablecoin token contract

---
+++ src/DecentralizedStableCoin.sol
@@ -0,0 +1,76 @@
+// SPDX-License-Identifier: MIT
+
+// errors
+// interfaces, libraries, contracts
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external
+// public
+// internal
+// private
+// view & pure functions
+
+pragma solidity ^0.8.20;
+
+import {ERC20Burnable, ERC20} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+/**
+ * @title DecentralizedStableCoin
+ * @author paulsimroth
+ *
+ * Collateral: Exogenous (wETH, wBTC)
+ * Minting: Algorithmic
+ * Relative Stability: Pegged to US$
+ *
+ * This contract is governed by DSCEngine
+ * This is an ERC20 Token for our Stablecoin
+ */
+contract DecentralizedStableCoin is ERC20Burnable, Ownable {
+    /**
+     * ERRORS
+     */
+    error DecentralizedStableCoin__MustBeMoreThanZero();
+    error DecentralizedStableCoin__BurnAmountExceedsBalance();
+    error DecentralizedStableCoin__NotZeroAddress();
+
+    /**
+     * CONSTRUCTOR
+     */
+    constructor() ERC20("DecentralizedStableCoin", "DSC") Ownable(msg.sender) {}
+
+    function burn(uint256 _amount) public override onlyOwner {
+        uint256 balance = balanceOf(msg.sender);
+        if (_amount <= 0) {
+            revert DecentralizedStableCoin__MustBeMoreThanZero();
+        }
+
+        if (balance < _amount) {
+            revert DecentralizedStableCoin__BurnAmountExceedsBalance();
+        }
+
+        super.burn(_amount);
+    }
+
+    function mint(
+        address _to,
+        uint256 _amount
+    ) external onlyOwner returns (bool) {
+        if (_to == address(0)) {
+            revert DecentralizedStableCoin__NotZeroAddress();
+        }
+        if (_amount <= 0) {
+            revert DecentralizedStableCoin__MustBeMoreThanZero();
+        }
+        _mint(_to, _amount);
+        return true;
+    }
+}
